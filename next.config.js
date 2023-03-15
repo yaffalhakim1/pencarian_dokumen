d36682536036b90ab3cf42d2db9eb05c361e0a59
yaffalhakim1 <yafi.tamfan08@gmail.com>
2023-03-15 14:15:22 +0700

    disable sw track in development

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
